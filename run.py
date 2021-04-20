0b181bac5f4e542a867e73e37701595fb19a8a3d
Aramantos <john.doyle.mail@icloud.com>
2021-04-20 13:20:31 +0000

    implemented bootstrap clean blog template

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -29,3 +30,5 @@ if __name__ == "__main__":
         port=int(os.environ.get("PORT", "5000")),
         debug=True
     )
+
+
